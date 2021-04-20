944b792f74d75a18e27acbe863ec44d2457e4dcd
Swdenis <swdenis@mail.ru>
2021-04-20 12:00:42 +0200

    Refactored CSV reader with composition

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { CsvFileReader } from "./CsvFileReader"
 import { MatchReader } from "./MatchReader"
 import { MatchResult } from "./MatchResult"
 
-const reader = new MatchReader('football.csv')
-reader.read()
+const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 let manUnitedWins = 0
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if(match[1] === 'Man United' && match[5]=== MatchResult.HomeWin) {
         manUnitedWins++
     } else if(match[2] === 'Man United' && match[5]=== MatchResult.AwayWin) {
@@ -16,4 +17,3 @@ for (let match of reader.data) {
 }
 
 console.log(`Man United won ${manUnitedWins} games`)
-console.log(reader.data[0][0])
