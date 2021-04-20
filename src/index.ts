ba12d92ce0dabc8a5cd29bbb407922c7512a6d23
Swdenis <swdenis@mail.ru>
2021-04-20 16:10:31 +0200

    Refactored the app with composition

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
+import { WinsAnalysis } from "./analyzers/WinsAnalysis"
 import { CsvFileReader } from "./CsvFileReader"
 import { MatchReader } from "./MatchReader"
 import { MatchResult } from "./MatchResult"
+import { ConsoleReport } from "./reportTargets/ConsoleReport"
+import { Summary } from "./Summary"
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-let manUnitedWins = 0
+const analyzer = new WinsAnalysis('Man United')
+const reporter = new ConsoleReport()
 
-for (let match of matchReader.matches) {
-    if(match[1] === 'Man United' && match[5]=== MatchResult.HomeWin) {
-        manUnitedWins++
-    } else if(match[2] === 'Man United' && match[5]=== MatchResult.AwayWin) {
-        manUnitedWins++
-    }
-}
+const summary = new Summary(analyzer,reporter)
 
-console.log(`Man United won ${manUnitedWins} games`)
+summary.buildAndPrintReport(matchReader.matches)
